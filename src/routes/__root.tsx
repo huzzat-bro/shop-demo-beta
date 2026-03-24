42e1f289c48823ce33b9071f4e11c92cafd9c0fe
Huzzat Hasan <shop.buybro@gmail.com>
2026-03-25 03:21:33 +0600

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,4 @@
 import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router"
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools"
-import { TanStackDevtools } from "@tanstack/react-devtools"
 
 import appCss from "../styles.css?url"
 import { TooltipProvider } from "@/components/ui/tooltip"
@@ -44,17 +42,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
             <Toaster />
           </CartProvider>
         </TooltipProvider>
-        <TanStackDevtools
-          config={{
-            position: "bottom-right",
-          }}
-          plugins={[
-            {
-              name: "Tanstack Router",
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-          ]}
-        />
         <Scripts />
       </body>
     </html>
